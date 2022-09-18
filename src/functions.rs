662b3e82d857657fc222a4732f038662b3053b81
Shahzaib Saleem <shazijutt987@gmail.com>
2022-09-18 22:22:38 +0500

    Rust functions

---
+++ src/functions.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, world! ");
+    another_function(5);
+}
+
+fn another_function(x:i32){
+    print!("---- Calling another function ---- \n");
+
+    print!("The value of x: {x}\n");
+    
+}
